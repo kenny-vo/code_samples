9c61d8d44130c8a0bdf9095bb91a51d61241c2ab
Kenneth Vo <kennyvo91@gmail.com>
2018-04-26 08:28:06 -0500

    progress bar code

--- code-snippets.ts
+++ code-snippets.ts
@@ -1,4 +1,7 @@
-// Array of an object with many different attributes.
+// Code used for progress bar logic with bootstrap progress bar.
+//If field is null or has not been filled out yet, it does not get added to total
+
+// object with many types of attributes
 var object = {
   key1: 'string',
   key2: '',
@@ -8,29 +11,38 @@ var object = {
   key6: 1,
 };
 
-
+// array of keys only relavent to progress bar
 var progressBarCriteria = [
-  object.key1,
   object.key2,
   object.key3,
-  object.key4,
   object.key5,
   object.key6
 ]
 
 // Calculate Progress Bar %
+
+// start progressbar count
 var progressCount = 1;
 var progressKeyLength = progressBarCriteria.length;
+
+// Loop through each key
 for (var i = 0; i<progressKeyLength; i++){
+  // Check if its an array
   if (!(progressBarCriteria[i] instanceof Array)){
+    // if it is not, check if its empty
     if(progressBarCriteria[i] !== null && progressBarCriteria[i] !== "") {
+      // add to count if it is not empty
       ++progressCount
     }
   } else {
+    // if its an array, check if if the array is empty
     let current = progressBarCriteria[i];
     if (Array.isArray(current) && current.length !== 0){
+      // add to count if not empty
       ++progressCount
     }
   }
 }
+
+// Calculate progress bar completion out of 100
 this.progressBar = ((100/progressKeyLength)*progressCount)
